d75a40e004c3c5eef31650a2f4344fe4fece48f3
Emerson Aagaard <emerson.aagaard@gmail.com>
2021-02-01 12:28:24 -0600

    working FAB for scroll-to-top

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -10,16 +10,59 @@ import {
   IconButton,
   Button,
   Toolbar,
+  useScrollTrigger,
+  Fab,
   // Icons,
 } from '@material-ui/core'
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import { makeStyles } from '@material-ui/core/styles';
+import Zoom from '@material-ui/core/Zoom';
+// import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import MenuIcon from '@material-ui/icons/Menu';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: 'fixed',
+    bottom: theme.spacing(2),
+    left: theme.spacing(2),
+  },
+}));
 
-function Nav() {
+// Scroll FAB
+function ScrollTop(props) {
+  const { children, window } = props;
+  const classes = useStyles();
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
 
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
+
+    if (anchor) {
+      anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
+  };
 
   return (
-    <div className='NavBar'>
+    <Zoom in={trigger}>
+      <div onClick={handleClick} role="presentation" className={classes.root}>
+        {children}
+      </div>
+    </Zoom>
+  );
+}
+
+function Nav(props) {
+
+  return (
+    <div className='NavBar'
+    id="back-to-top-anchor">
       <AppBar>
         <Toolbar>
 
@@ -80,6 +123,13 @@ function Nav() {
         </Toolbar>
       </AppBar>
       <Toolbar/>
+
+      <ScrollTop {...props}>
+        <Fab color="secondary" size="small" aria-label="scroll back to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop>
+
     </div>
   );
 }
